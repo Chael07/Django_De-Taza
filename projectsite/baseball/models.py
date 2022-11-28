ed5f55d71946cf9175051ddefe940956aae5bcee
unknown <202080469@psu.palawan.edu.ph>
2022-11-28 13:11:50 +0800

    updated'

--- projectsite/baseball/models.py
+++ projectsite/baseball/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.utils import timezone
 
-class BaseModel(models.Model):
+# Create your models here.
+
+class BaseModel (models.Model):
     created_at = models.DateTimeField(
-        auto_now_add=True, db_index=True)
+        auto_now_add=True, db_index=True
+    )
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
@@ -19,7 +22,6 @@ class Position(BaseModel):
 class Person(BaseModel):
     lastname = models.CharField(max_length=100)
     firstname = models.CharField(max_length=100)
-    email = models.EmailField(null=True, blank=True, max_length=100)
     height = models.DecimalField(max_digits=10, decimal_places=5, null=True)
     weight = models.DecimalField(max_digits=10, decimal_places=5, null=True)
 
@@ -30,7 +32,6 @@ class Person(BaseModel):
 class Club(BaseModel):
     name = models.CharField(max_length=100)
     coach = models.ForeignKey(Person, on_delete=models.CASCADE)
-    description = models.TextField(null=True, blank=True)
     dorm_latitude = models.DecimalField(max_digits=22, decimal_places=16, null=True, blank=True)
     dorm_longitude = models.DecimalField(max_digits=22, decimal_places=16, null=True, blank=True)
     team_pic = models.ImageField(default="defaultimg.png", null=True, blank=True, verbose_name="Team Image")
@@ -38,13 +39,12 @@ class Club(BaseModel):
     def __str__(self):
         return f"{self.name}"
 
-
 class Play(BaseModel):
     STRING_CHOICES= (
     ('First String', 'First String'),
     ('Second String','Second String'))
     player = models.ForeignKey(Person, on_delete=models.CASCADE, related_name = "Player")
-    team = models.ForeignKey(Club, on_delete=models.CASCADE, related_name = "Team")
+    team = models.ForeignKey(Person, on_delete=models.CASCADE, related_name = "Team")
     string_no = models.CharField(max_length=100, choices = STRING_CHOICES)
     isActive = models.BooleanField(default=False)
     pos = models.ForeignKey(Position, on_delete=models.CASCADE)
@@ -59,3 +59,5 @@ class Match(BaseModel):
 
     def __str__(self):
         return f"{self.team1} vs {self.team2}"
+
+
